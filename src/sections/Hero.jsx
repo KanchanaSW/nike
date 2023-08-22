da1711162edb1739a84b53e48d7641646011b64a
Kanchana Walagambahu <kanchana.walagambahu@axiatadigitallabs.com>
2023-08-22 16:27:56 +0530

    hero btn done

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,24 @@
+import Button from "../components/Button"
+import {arrowRight} from '../assets/icons'
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="Home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28">
+        <p>
+          Our Summer Collection
+        </p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike</span>
+          Shoes
+        </h1>
+        <p>Discover stylish Nike arrivals, quality comfort, and innovations for your active life.</p>
+        <Button label='Show now' iconURL={arrowRight}/>
+      </div>
+    </section>
+
   )
 }
 
